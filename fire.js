35f03c7d3596b359e4544d941ad68ea1bb298abe
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-09-05 10:50:23 +0200

    Adds fire width and fire height. Starts work on createFireDataStructure() function

--- fire.js
+++ fire.js
@@ -1,13 +1,21 @@
+const firePixelsArray = []
+const fireWidth = 2
+const fireHeight = 3
+
 function start() {
     
 }
 
 function createFireDataStructure() {
-
+  const numberOfPixels = fireWidth * fireHeight;
 }
 
 function calculateFirePropagation() {
-    
+
+}
+
+function renderFire() {
+
 }
 
 start()
