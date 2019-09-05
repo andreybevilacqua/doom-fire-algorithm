116ecfc698d3510b2319d7d196bfbeddda7b4699
Andrey Bevilacqua <andreybevilacqua@gmail.com>
2019-09-05 12:22:37 +0200

    Sets the color of each pixel and show that color in the table

--- fire.js
+++ fire.js
@@ -46,6 +46,7 @@ function updateFireIntensityPerPixel(currentPixelIndex) {
 }
 
 function renderFire() {
+  const debug = false
   let html = '<table cellpadding=0 cellspacing=0>'
 
   for(let row = 0; row < fireHeight; row++ ) {
@@ -55,10 +56,17 @@ function renderFire() {
       const pixelIndex = column + (fireWidth * row)
       const fireIntensity = firePixelsArray[pixelIndex]
       
-      html += '<td>'
-      html += `<div class="pixel-index">${pixelIndex}</div>`
-      html += fireIntensity;
-      html += '</td>'
+      if(debug === true) {
+        html += '<td>'
+        html += `<div class="pixel-index">${pixelIndex}</div>`
+        html += fireIntensity;
+        html += '</td>'
+      } else {
+        const color = fireColorsPalette[fireIntensity]
+        const colorString = `${color.r},${color.g},${color.b}`
+        html += `<td style="background-color: rgb(${colorString})">`
+        html += '</td>'
+      }
     }
 
     html += '</tr>'
